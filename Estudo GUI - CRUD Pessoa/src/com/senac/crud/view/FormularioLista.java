9890ce37a3aee796d7485ec7bd1138f7da7b042d
Dantieris Castilhos Rabelini <dantieris@gmail.com>
2014-09-16 11:13:22 -0300

    Etudo do GridBagLayout, JList e CRUD Pessoa.

---
+++ Estudo GUI - CRUD Pessoa/src/com/senac/crud/view/FormularioLista.java
@@ -0,0 +1,124 @@
+package com.senac.crud.view;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.EventListener;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+
+import com.senac.crud.model.Pessoa;
+
+public class FormularioLista extends JFrame {
+	
+	private DefaultListModel<Pessoa> modelList;
+	private JList<Pessoa> lt_lista;
+	private JButton bt_novo, bt_editar, bt_remover;
+	
+	public FormularioLista() {
+		super("CRUD Pessoa");		
+		this.setLocation(400,200);
+		this.setSize(280,200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new GridBagLayout());
+		
+		iniciarComponentes();
+		
+		this.setVisible(true);
+	}
+
+	private void iniciarComponentes() {
+		// Configurando o Layout
+		GridBagConstraints c = new GridBagConstraints();
+		
+		modelList = new DefaultListModel<Pessoa>();
+		lt_lista = new JList<Pessoa>(modelList);
+		
+		// Adicionando a lista ao JFrame
+		lt_lista.setSize(200, 200);
+		
+		c.fill = GridBagConstraints.BOTH;
+		
+		c.ipadx = 0;
+		c.ipady = 0;
+		
+		c.weightx = 100;
+		c.weighty = 100;		
+		
+		c.anchor = GridBagConstraints.NORTH;
+		
+	    c.gridheight = 1;
+	    c.gridwidth = 3;
+	    
+	    c.gridx = 0;
+	    c.gridy = 0;
+		
+		this.add(lt_lista, c);
+		
+		// Adicionando o botão novo ao JFrame
+		bt_novo = new JButton("Novo");
+		
+		c.fill = GridBagConstraints.NONE;
+		
+		c.weightx = 50;
+		c.weighty = 50;		
+		
+		c.anchor = GridBagConstraints.LINE_START;
+		
+	    c.gridheight = 1;
+	    c.gridwidth = 1;
+	    
+	    c.gridx = 0;
+	    c.gridy = 1;
+		
+		this.add(bt_novo, c);
+		
+		// Adicionando o botão editar ao JFrame
+		bt_editar = new JButton("Editar");
+		
+		c.weightx = 50;
+		c.weighty = 50;		
+		
+		c.anchor = GridBagConstraints.CENTER;
+		
+	    c.gridheight = 1;
+	    c.gridwidth = 1;
+	    
+	    c.gridx = 1;
+	    c.gridy = 1;
+		
+		this.add(bt_editar, c);
+		
+		// Adicionando o botão remover ao JFrame
+		bt_editar = new JButton("Remover");
+		
+		c.weightx = 50;
+		c.weighty = 50;		
+		
+		c.anchor = GridBagConstraints.LINE_END;
+		
+	    c.gridheight = 1;
+	    c.gridwidth = 1;
+	    
+	    c.gridx = 2;
+	    c.gridy = 1;
+		
+		this.add(bt_editar, c);
+		
+		// Adicionando evento aos botões
+		bt_novo.addActionListener(null);
+	}
+	
+	class Eventos implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
+		
+}
---
+++ Estudo GUI - CRUD Pessoa/src/com/senac/crud/view/FormularioPessoa.java
@@ -0,0 +1,105 @@
+package com.senac.crud.view;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class FormularioPessoa extends JFrame {
+	
+	private JTextField tf_nome, tf_idade;
+	private JButton bt_ok, bt_cancelar;
+	
+	public FormularioPessoa() {
+		super("Formulário Pessoa");		
+		this.setLocation(400,200);
+		this.setSize(280,200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new GridBagLayout());
+		
+		iniciarComponentes();
+		
+		this.setVisible(true);
+	}
+
+	private void iniciarComponentes() {
+		GridBagConstraints c = new GridBagConstraints();
+		
+		// Adicionando o label nome ao painel.
+		JLabel lb_nome 	= new JLabel("Nome:");
+		
+		c.weightx = 50;
+		c.weighty = 50;
+		
+		
+		
+		c.anchor = GridBagConstraints.WEST;
+		
+	    c.gridheight = 1;
+	    c.gridwidth = 1;
+	    
+	    c.gridx = 1;
+	    c.gridy = 1;
+	    
+	    this.add(lb_nome, c);
+		
+	    // Adicionando o texto nome ao painel.
+		tf_nome = new JTextField(15);
+		
+	    c.gridheight = 1;
+	    c.gridwidth = 2;
+	    
+	    
+	    c.gridx = 2;
+	    c.gridy = 1;
+	    
+	    this.add(tf_nome, c);
+	    
+		// Adicionando o label idade ao painel.
+		JLabel lb_idade = new JLabel("Idade:");
+		
+		c.gridheight = 1;
+		c.weightx = 1;
+	    c.gridx = 1;
+	    c.gridy = 2;
+	    
+		this.add(lb_idade, c);
+	    // Adicionando o texto idade ao painel.
+		tf_idade = new JTextField(15);
+		
+		c.gridheight = 1;
+	    c.gridwidth = 2;
+	    
+	    c.gridx = 2;
+	    c.gridy = 2;
+	    
+	    this.add(tf_idade, c);
+	    // Adicionando os botões ao painel
+	    bt_ok 		= new JButton("OK");
+	    bt_cancelar = new JButton("Cancelar");
+	    
+	    c.anchor = GridBagConstraints.CENTER;
+	    
+		c.gridheight = 1;
+		c.weightx = 1;
+	    
+		c.gridx = 1;
+	    c.gridy = 5;
+	    
+	    this.add(bt_ok, c);
+	    
+	    
+	    
+		c.gridheight = 1;
+		c.weightx = 1;
+		
+	    c.gridx = 2;
+	    c.gridy = 5;
+	    
+	    this.add(bt_cancelar, c);
+	    
+	}
+}
